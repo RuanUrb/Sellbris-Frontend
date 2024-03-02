8ddd07f849b9a90b84a032e78cdbe9d135904811
RuanUrb <ruaurb@alunos.utfpr.edu.br>
2024-03-02 03:19:42 -0300

    feat: add global style and custom themes.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { UserStorage } from './context/UserContext.tsx'
+import { AuthProvider } from './context/AuthContext.tsx'
+import { GlobalStyle } from './styles/globalStyles.ts'
+import { Theme } from './styles/Theme.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <UserStorage>
+    <AuthProvider>
+      <Theme>
+        <App />
+        <GlobalStyle />
+      </Theme>
+    </AuthProvider>
+    </UserStorage>
   </React.StrictMode>,
 )
