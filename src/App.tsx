25f0722b47d311db9dc6bd72cf1b1c30fa31f271
RuanUrb <ruaurb@alunos.utfpr.edu.br>
2024-03-02 03:31:37 -0300

    feat: add basic routing structure to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,48 @@
-import styled from 'styled-components'
-import { Routes, Route } from 'react-router-dom'
-import '/App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home/Home";
+import './App.css'
+import Header from "@/layout/Header";
+import Footer from "@/layout/Footer/index";
+import styled from 'styled-components';
+import ProductInfo from "@/components/ProductInfo";
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
+import Sell from "./pages/Sell";
+import Products from './pages/Products'
+import Wishlist from "@/pages/Cart";
+import SignOut from "./pages/Signout";
 
-const Container = styled.div`
+const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
   overflow-x: hidden;
 `
 
+const Content = styled.div`
+  flex: 1;
+`
 function App() {
-
   return (
     <>
-        <Container>
-          <Routes>
-            <Header/>
-            {/*
-
-              DEFINE ROUTES : '/', '/PRODUCT-12315847', 'FORM TO PRODUCT', 'FORM TO REGISTER', 'FORM TO LOGIN', 'COMMENT SECTION', 'USER-489478213' 
-
-            */ }
-          </Routes>
-        </Container>      
+      <MainContainer>
+        <Header />
+          <Content>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="products/:id" element={<ProductInfo />} />
+                <Route path="/register" element={<Register/>} />
+                <Route path="/signin" element={<SignIn/>} />
+                <Route path="/signout" element={<SignOut/>} />
+                <Route path="/sell" element={<Sell/>} />
+                <Route path="/products" element={<Products/>} />
+                <Route path="/wishlist" element={<Wishlist/>} />
+            </Routes>
+          </Content>
+        <Footer />
+      </MainContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
