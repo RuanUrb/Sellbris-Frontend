e8b98ccffd931f629aa40d11da35cd561bc76091
RuanUrb <ruaurb@alunos.utfpr.edu.br>
2024-03-02 03:30:49 -0300

    chore: envelopes main by client side routing

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,17 @@ import { UserStorage } from './context/UserContext.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
 import { GlobalStyle } from './styles/globalStyles.ts'
 import { Theme } from './styles/Theme.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <UserStorage>
     <AuthProvider>
       <Theme>
-        <App />
-        <GlobalStyle />
+        <BrowserRouter>
+          <App />
+          <GlobalStyle />
+        </BrowserRouter>
       </Theme>
     </AuthProvider>
     </UserStorage>
